cd705de9925224ef1931863c17d4f611bc0f99a3
chrisss101 <Crafaniello9535@bths.edu>
2018-09-18 10:14:47 -0400

    Crafaniello

--- src/crafaniello/crafaniellolib.java
+++ src/crafaniello/crafaniellolib.java
@@ -6,4 +6,57 @@ public class crafaniellolib {
        System.out.println(str);
     }
 
+    public static Boolean isPalindrome(String str1) {
+        String newStr = "";
+        for (int i = str1.length(); i >= 0; i-- ) {
+            String addedString = str1.substring(i);
+            newStr = newStr + addedString;
+        }
+        if (str1 == newStr){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static int sumUpTo( int num) {
+        int sum = 0;
+        for (int i = 0; i <= num; i++) {
+            sum = sum + i;
+        }
+        return sum;
+
+
+    }
+
+    public static void fooBarBaz( int num1) {
+        String storage = "";
+        for ( int i = 0; i <= num1; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
+                storage = storage + "baz ";
+            }
+            else if ((i % 3) == 0) {
+                storage = storage + "foo ";
+            }
+            else if ((i % 5) == 0) {
+                storage = storage + "bar ";
+            }
+            else {
+                storage = storage + i;
+            }
+        }
+        System.out.print(storage);
+    }
+
+    public static void multiplicationTable (int base, int range) {
+        String table = "";
+        for (int x = 0; x <= range; x++) {
+            int next = base * x;
+            table = table + next + " ";
+        }
+        System.out.println(table);
+    }
+
+
 }
--- src/crafaniello/testfile.java
+++ src/crafaniello/testfile.java
@@ -1,11 +1,19 @@
 package crafaniello;
 
 import static crafaniello.crafaniellolib.println;
+import static crafaniello.crafaniellolib.multiplicationTable;
 
 public class testfile {
 
+   // public static void main(String[] args) {
+       // println("thetrees");
+   // }
     public static void main(String[] args) {
-        println("thetrees");
+        multiplicationTable(7,10);
     }
 
+
+
 }
+
+
