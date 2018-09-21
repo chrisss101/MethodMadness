35d84dc7eac7dec73834e451a63ed72741693e59
chrisss101 <Crafaniello9535@bths.edu>
2018-09-21 10:02:49 -0400

    erer

--- src/crafaniello/crafaniellolib.java
+++ src/crafaniello/crafaniellolib.java
@@ -3,10 +3,15 @@ package crafaniello;
 
 public class crafaniellolib {
 
-    public static void println(String str) {
+    /*/public static void println(String str) {
        System.out.println(str);
-    }
+    } /*/
 
+    /**
+     *
+     * @param str1 String input that the function decides whether it is a palindrome or not
+     * @return true if str1 is a palindrome(spelled same way forwards or backwards), and false otherwise
+     */
     public static Boolean isPalindrome(String str1) {
         String newStr = "";
         for (int i = str1.length(); i >= 0; i-- ) {
@@ -21,6 +26,11 @@ public class crafaniellolib {
         }
     }
 
+    /**
+     *
+     * @param num the upper limit of numbers the function uses
+     * @return all of the numbers up to num added together
+     */
     public static int sumUpTo( int num) {
         int sum = 0;
         for (int i = 0; i <= num; i++) {
@@ -31,6 +41,10 @@ public class crafaniellolib {
 
     }
 
+    /**
+     *
+     * @param num1 the upper limit of numbers for which the function detemines whether it is foo, bar or baz
+     */
     public static void fooBarBaz( int num1) {
         String storage = "";
         for ( int i = 1; i <= num1; i++) {
@@ -50,6 +64,11 @@ public class crafaniellolib {
         System.out.print(storage);
     }
 
+    /**
+     *
+     * @param base a number that gets multiplied by each value of x
+     * @param range defines the upper limit of numbers the function uses
+     */
     public static void multiplicationTable (int base, int range) {
         String table = "";
         for (int x = 0; x <= range; x++) {
@@ -59,7 +78,11 @@ public class crafaniellolib {
         System.out.println(table);
     }
 
-
+    /**
+     *
+     * @param date a String taht represents a date in the form mm-dd-year
+     * @return the date in the form dd-mm-year
+     */
     public static String dateString(String date) {
         String first = date.substring(0,2);
         String second = date.substring(3,5);
